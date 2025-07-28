af242ca593d3b6a4ed4fe0f88bc8eab2603fd200
ASCSagar <ascsagar08@gmail.com>
2025-07-28 16:24:47 +0530

    GA-GSC-GAA-Code-Script

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,44 @@
 import "./globals.css";
+import Script from "next/script";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-17WCD38LSF"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag() {
+                dataLayer.push(arguments);
+              }
+              gtag('js', new Date());
+              gtag('config', 'G-17WCD38LSF');
+            `,
+          }}
+        ></Script>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2283636579212292"
+          crossorigin="anonymous"
+        ></Script>
+        <meta
+          name="google-site-verification"
+          content="y3KM-P7eNzRGw6fC13PDdBfeOP776KhdOvcCP1oXTkk"
+        ></meta>
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-2283636579212292"
+        ></meta>
+      </head>
       <body>
         <Header />
         {children}
